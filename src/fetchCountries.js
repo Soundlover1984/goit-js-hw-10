f0c55c9f86a864216d5d6135d1d541dac15ca574
Soundlover1984 <denys.kovtun@gmail.com>
2023-04-15 12:35:41 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,17 @@
-const URL_BASE = "https://restcountries.com/v3.1/name/";
+const REST_URL = "https://restcountries.com/v3.1/name/";
 
 export function fetchCountries(name) {
-  return fetch(`${URL_BASE}${name}?fields=name,capital,population,flags,languages`)
+  return fetch(`${REST_URL}${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
       if (response.status === 404) {
         throw new Error(response.status);
       } 
       return response.json();
     })
-    .catch(e => console.error(e));
+    .catch(e => {
+      console.error(e);
+      throw e;
+    });
 }
 
 
@@ -27,16 +30,3 @@ export function fetchCountries(name) {
 
 
 
-
-// const BASE_URL = 'https://restcountries.com/v3.1/name/';
-
-// export function fetchCountries(name) {
-//   return fetch(
-//     `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
-//   ).then(response => {
-//     if (response.status === 404) {
-//       return Promise.reject();
-//     }
-//     return response.json();
-//   });
-// }
