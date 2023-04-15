4bc6f8cae8d7a9908a70233a7017d577d0d6147a
Soundlover1984 <denys.kovtun@gmail.com>
2023-04-15 11:52:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,4 +41,30 @@ const refs = {
     }, DEBOUNCE_DELAY)
   );
   
+  function renderCountryList(country) {
+    const markup = country
+      .map(countries => {
+        return `<li>
+          <img src="${countries.flags.svg}" alt="Flag of ${countries.name.official}" width="100" hight="50">
+          <br><b>${countries.name.official}</b>
+          </li>`;
+      })
+      .join('');
+    refs.countryList.innerHTML = markup;
+  }
+  
+  function renderCountryInfo(countries) {
+    const markup = countries
+      .map(country => {
+        return `<img src="${country.flags.svg}" alt="Flag of ${
+          country.name.official}" width="200" hight="100">
+          <p style="font-size: 25px; text-shadow: 1px 1px 2px grey"><b>${country.name.official}</b></p>
+          <p>Capital: <b>${country.capital}</b></p>
+          <p>Population:<b>${country.population}</b></p>
+          <p>Languages: <b>${Object.values(country.languages)}</b></p>`;
+      })
+      .join('');
+    refs.countryInfo.innerHTML = markup;
+  }
+  
   
