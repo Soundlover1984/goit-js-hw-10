be2f1e98d08b72d4e087994fb189ad898bee51b0
Soundlover1984 <denys.kovtun@gmail.com>
2023-04-15 10:44:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,26 +11,31 @@ const refs = {
     countryInfo: document.querySelector('.country-info'),
   };
 
+  function cleanHtmlList() {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+  }
+  
+
   refs.inputData.addEventListener(
-    'input',
-    debounce(e => {
-      const trimValue = refs.inputData.value.trim();
+    'input', debounce(e => {
+      const inputValue = refs.inputData.value.trim();
   
       cleanHtmlList();
   
-      if (trimValue !== '') {
-        fetchCountries(trimValue).then(result => {
+      if (inputValue !== '') {
+        fetchCountries(inputValue).then(result => {
           if (result.length > 10) {
             Notiflix.Notify.info(
               'Too many matches found. Please enter a more specific name.'
             );
-          } else if (result.length === 0) {
-            Notiflix.Notify.failure('Oops, there is no country with that name');
           } else if (result.length >= 2 && result.length <= 10) {
             renderCountryList(result);
           } else if (result.length === 1) {
             renderCountryInfo(result);
           }
+        }).catch(error => {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
         });
       }
     }, DEBOUNCE_DELAY)
